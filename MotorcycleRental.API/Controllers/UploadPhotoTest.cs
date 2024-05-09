42fb36cc2e83ecad77d3443d9948a7cb3737dc9e
amaurileal <amaurileal@hotmail.com.br>
2024-05-09 11:20:54 -0300

    parcialmente feito

---
+++ MotorcycleRental.API/Controllers/UploadPhotoTest.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MotorcycleRental.Application.Bikers.Commands;
+
+namespace MotorcycleRental.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadPhotoTest(IMediator mediator) : ControllerBase
+    {
+        [HttpPost("upload")]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UploadImage([FromForm] SendCNHImageCommand command)
+        {
+            var result = await mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
---
+++ MotorcycleRental.Application/Bikers/Commands/SendCNHImageCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using Amazon.S3.Model;
+using Amazon.S3;
+using MediatR;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MotorcycleRental.Application.Bikers.Commands
+{
+    public class SendCNHImageCommandHandler(IAmazonS3 s3Client) : IRequestHandler<SendCNHImageCommand, bool>
+    {
+        private const string bucketName = "motorcycle-rent-temp";
+        public async Task<bool> Handle(SendCNHImageCommand request, CancellationToken cancellationToken)
+        {
+
+            if (request.CNHImage == null || (request.CNHImage.ContentType != "image/png" && request.CNHImage.ContentType != "image/bmp"))
+            {
+                throw new Exception("Invalid file format. Only PNG and BMP files are allowed.");
+            }
+            var key = $"{Path.GetFileNameWithoutExtension(request.CNHImage.FileName)}-{Guid.NewGuid()}{Path.GetExtension(request.CNHImage.FileName)}";
+
+            using (var stream = new MemoryStream())
+            {
+                await request.CNHImage.CopyToAsync(stream);
+                stream.Position = 0;
+                var requestAWS = new PutObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = key,
+                    InputStream = stream,
+                    ContentType = request.CNHImage.ContentType
+                };
+                await s3Client.PutObjectAsync(requestAWS);
+            }
+
+            return true;
+
+        }
+    }
+}
